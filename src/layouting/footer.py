85c6b5835f941867e37015104ae2d4891c58e396
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2025-06-08 15:10:59 +0700

    Perbarui informasi pengembang di komponen footer untuk mencantumkan nomor induk mahasiswa yang benar.

--- src/layouting/footer.py
+++ src/layouting/footer.py
@@ -8,6 +8,6 @@ def render_footer():
         <hr style='margin-top:32px;margin-bottom:8px;'>
         <div style='text-align:center;font-size:0.95rem;color:#888;'>
             &copy; 2025 RS Juliana | Dibangun dengan Streamlit<br>
-            <span style='font-size:0.93rem;color:#888;'>Developer: Eko Muchamad Haryono (Ketua Tim) &amp; Raka Muhammad Rabbani - 0110223050</span>
+            <span style='font-size:0.93rem;color:#888;'>Developer: Eko Muchamad Haryono - 0110223079 (Ketua Tim) &amp; Raka Muhammad Rabbani - 0110223050</span>
         </div>
     """, unsafe_allow_html=True)
